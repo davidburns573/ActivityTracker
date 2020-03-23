f89d4f9da292656c878c8f835725fac8a9dfc56d
David Burns <davidburns573@gmail.com>
2020-03-23 10:39:40 -0500

    Finished ActivityDatabase

--- app/src/main/java/tech/davidburns/activitytracker/User.kt
+++ app/src/main/java/tech/davidburns/activitytracker/User.kt
@@ -8,18 +8,6 @@ import com.google.firebase.auth.FirebaseUser
 import tech.davidburns.activitytracker.util.ActivityBaseHelper
 import tech.davidburns.activitytracker.util.ActivitySchema
 
-//class User(var name: String) {
-//    constructor(firebaseUser: FirebaseUser) : this(
-//        firebaseUser.displayName ?: "UNNAMED"
-//    )
-//
-//    var activities: MutableList<Activity> = mutableListOf()
-//
-//    fun addActivity(name: String) {
-//        activities.add(Activity(name))
-//    }
-//}
-
 object User {
     var authenticate: FirebaseUser? = null
     var name: String = "UNNAMED"
@@ -49,8 +37,7 @@ object User {
             whereArgs,
             null,
             null,
-            null
-        )
+            null)
         return ActivityCursorWrapper(cursor)
     }
 
