fe34f4b411033e551601e2f4dc9bed8091bdf9d9
unknown <sandiegocharlie.gmail.com>
2020-05-08 13:38:53 -0700

    Removed Unit Tests

--- app/src/test/java/tech/davidburns/activitytracker/ExampleUnitTest.kt
+++ app/src/test/java/tech/davidburns/activitytracker/ExampleUnitTest.kt
@@ -13,19 +13,19 @@ import java.time.LocalDate
  * See [testing documentation](http://d.android.com/tools/testing).
  */
 class ExampleUnitTest {
-    @Test
-    fun addition_isCorrect() {
-        assertEquals(4, 2 + 2)
-    }
-
-    @Test
-    fun addingToList() {
-        val list: MutableList<Int> = mutableListOf()
-        list.add(3, 3)
-        list.add(1,1)
-        list.add(2,2)
-        Log.i("TESTINGTEST", list.toString())
-    }
+//    @Test
+//    fun addition_isCorrect() {
+//        assertEquals(4, 2 + 2)
+//    }
+//
+//    @Test
+//    fun addingToList() {
+//        val list: MutableList<Int> = mutableListOf()
+//        list.add(3, 3)
+//        list.add(1,1)
+//        list.add(2,2)
+//        Log.i("TESTINGTEST", list.toString())
+//    }
 
 //    @Test
 //    fun aliasing() {
@@ -47,36 +47,36 @@ class ExampleUnitTest {
 //        }
 //    }
 
-    fun timeLastWeek(sessions: MutableList<Session>): Array<Duration> {
-        val lastSevenDays: Array<Duration> = Array(7) { Duration.ZERO }
-        val now: LocalDate = LocalDate.now()
-
-        sessions.forEach {
-            when(Duration.between(it.start, now).toDays().toInt()) {
-                0 -> lastSevenDays[6] = lastSevenDays[6].plus(it.length)
-                1 -> lastSevenDays[5] = lastSevenDays[5].plus(it.length)
-                2 -> lastSevenDays[4] = lastSevenDays[4].plus(it.length)
-                3 -> lastSevenDays[3] = lastSevenDays[3].plus(it.length)
-                4 -> lastSevenDays[2] = lastSevenDays[2].plus(it.length)
-                5 -> lastSevenDays[1] = lastSevenDays[1].plus(it.length)
-                6 -> lastSevenDays[0] = lastSevenDays[0].plus(it.length)
-            }
-        }
-
-        return lastSevenDays
-    }
-
-    fun timeLastWeekShort(sessions: MutableList<Session>): Array<Duration> {
-        val lastSevenDays: Array<Duration> = Array(7) { Duration.ZERO }
-        val now: LocalDate = LocalDate.now()
-
-        sessions.forEach {
-            val duration = Duration.between(it.start, now).toDays().toInt()
-            lastSevenDays[6 - duration] += it.length
-        }
-
-        return lastSevenDays
-    }
+//    fun timeLastWeek(sessions: MutableList<Session>): Array<Duration> {
+//        val lastSevenDays: Array<Duration> = Array(7) { Duration.ZERO }
+//        val now: LocalDate = LocalDate.now()
+//
+//        sessions.forEach {
+//            when(Duration.between(it.start, now).toDays().toInt()) {
+//                0 -> lastSevenDays[6] = lastSevenDays[6].plus(it.length)
+//                1 -> lastSevenDays[5] = lastSevenDays[5].plus(it.length)
+//                2 -> lastSevenDays[4] = lastSevenDays[4].plus(it.length)
+//                3 -> lastSevenDays[3] = lastSevenDays[3].plus(it.length)
+//                4 -> lastSevenDays[2] = lastSevenDays[2].plus(it.length)
+//                5 -> lastSevenDays[1] = lastSevenDays[1].plus(it.length)
+//                6 -> lastSevenDays[0] = lastSevenDays[0].plus(it.length)
+//            }
+//        }
+//
+//        return lastSevenDays
+//    }
+//
+//    fun timeLastWeekShort(sessions: MutableList<Session>): Array<Duration> {
+//        val lastSevenDays: Array<Duration> = Array(7) { Duration.ZERO }
+//        val now: LocalDate = LocalDate.now()
+//
+//        sessions.forEach {
+//            val duration = Duration.between(it.start, now).toDays().toInt()
+//            lastSevenDays[6 - duration] += it.length
+//        }
+//
+//        return lastSevenDays
+//    }
 //    @Test
 //    fun sessionDatabase() {
 //        val activity = Activity("David")
